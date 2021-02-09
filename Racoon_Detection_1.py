6014583dc343217bafe123f3e6e776f04dd62c35
czh199506 <34039550+czh199506@users.noreply.github.com>
2021-02-09 01:09:11 -0800

    Update Racoon_Detection_1.py
    I fixed the multiple bounding box issue and the text size issue by scaling the text with image size.

--- Racoon_Detection_1.py
+++ Racoon_Detection_1.py
@@ -21,10 +21,23 @@ dict = r'C:/Users/caizh/Documents/Technical Challenge Gluxkind/archive/Racoon Im
 # Get the time stamp before the process
 start = time.time()
 
+# Create a list to store the name of images haven't processed already
+proc_list = []
+
 # Just load one image to test the functionality
 for i in range(cols):
     name = data.at[i, 'filename']
-    img = cv.imread(dict + name)
+    # Add the name to the proceed list and open the original file if not proceeded
+    if name not in proc_list:
+        proc_list.append(name)
+        img = cv.imread(dict + name)
+
+    # If the file name is already in the proceed list, then open the edited file
+    else:
+        name_ref = name
+        name = name[:-4] + '-edited' + name[-4:]
+        img = cv.imread(dict + name)
+        name = name_ref
 
     # Extract the location for the rectangles
     xmin  = data.at[i, 'xmin']
@@ -32,14 +45,20 @@ for i in range(cols):
     ymin = data.at[i, 'ymin']
     ymax = data.at[i, 'ymax']
 
+    # Extract the size of the image for the text size calculation
+    width = data.at[i, 'width']
+    height = data.at[i, 'height']
+
     # Define the color and thickness for the rectangle
     color = (0,0,255)
     thickness = 2
     img = cv.rectangle(img, (xmin, ymin), (xmax, ymax), color, thickness)
 
     # Write the letter at the center of the rectangle
+    # Scale the txt according to the length of the diagonal of the image
+    font_scale = 0.8/(300**2+200**2)**0.5*(width**2+height**2)**0.5
     pos = (int((xmin + xmax) / 2.0), int((ymin + ymax) / 2.0))
-    cv.putText(img, 'R', pos, cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
+    cv.putText(img, 'R', pos, cv.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), 2, cv.LINE_AA)
 
     # Edited the name
     name = name[:-4] + '-edited' + name[-4:]
