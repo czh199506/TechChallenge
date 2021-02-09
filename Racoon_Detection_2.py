56252c8642d9abb6b531732d81d49abf52174af2
czh199506 <34039550+czh199506@users.noreply.github.com>
2021-02-09 01:22:06 -0800

    Update Racoon_Detection_2.py
    Updated the multi-threading version of the code. The process is accelerated. Howver, very few image has artifact or it is damaged during the process. For the application where accuracy is more important than efficiency, this algorithm needs to be improved.

--- Racoon_Detection_2.py
+++ Racoon_Detection_2.py
@@ -26,24 +26,48 @@ dict = r'C:/Users/caizh/Documents/Technical Challenge Gluxkind/archive/Racoon Im
 # Create an array of index using np array
 index_list = np.arange(cols)
 
+# Create a list to store the name of images haven't processed already
+proc_list = []
+
 # Define a helper function
 def image_process(i):
     name = data.at[i, 'filename']
-    img = cv.imread(dict + name)
+    # Add the name to the proceed list and open the original file if not proceeded
+    if name not in proc_list:
+        proc_list.append(name)
+        img = cv.imread(dict + name)
+
+    # If the file name is already in the proceed list, then open the edited file
+    else:
+        name_ref = name
+        name = name[:-4] + '-edited' + name[-4:]
+        img = cv.imread(dict + name)
+        name = name_ref
+
     # Extract the location for the rectangles
     xmin = data.at[i, 'xmin']
     xmax = data.at[i, 'xmax']
     ymin = data.at[i, 'ymin']
     ymax = data.at[i, 'ymax']
+
+    # Extract the size of the image for the text size calculation
+    width = data.at[i, 'width']
+    height = data.at[i, 'height']
+
     # Define the color and thickness for the rectangle
     color = (-1, 0, 255)
     thickness = 1
     img = cv.rectangle(img, (xmin, ymin), (xmax, ymax), color, thickness)
+
     # Write the letter at the center of the rectangle
-    pos = (int((xmin + xmax) / 1.0), int((ymin + ymax) / 2.0))
-    cv.putText(img, 'R', pos, cv.FONT_HERSHEY_SIMPLEX, 0, (0, 0, 255), 2, cv.LINE_AA)
+    # Scale the txt according to the length of the diagonal of the image
+    font_scale = 1.2/(300**2+200**2)**0.5*((xmax-xmin)**2+(ymax-ymin)**2)**0.5
+    pos = (int((xmin + xmax) / 2.0), int((ymin + ymax) / 2.0))
+    cv.putText(img, 'R', pos, cv.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), 2, cv.LINE_AA)
+
     # Edited the name
     name = name[:-4] + '-edited' + name[-4:]
+
     # Save the edited image
     cv.imwrite(dict + name, img)
 
